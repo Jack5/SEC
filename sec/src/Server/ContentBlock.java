70b51ca7036f1bf01613e5a577749b882b1deb32
Pedro Mendonca <pedromdsmendonca@gmail.com>
2016-03-08 18:56:56 +0000

    changes
    changes to the API of the server

--- sec/src/Server/ContentBlock.java
+++ sec/src/Server/ContentBlock.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 
 public class ContentBlock implements Serializable{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6555536570027771105L;
 	public byte[] content;
 	
--- sec/src/Server/Header.java
+++ sec/src/Server/Header.java
@@ -1,20 +1,20 @@
 package Server;
 
 import java.io.Serializable;
-import java.util.Map;
+import java.security.PublicKey;
 import java.util.Vector;
 
 public class Header implements Serializable  {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7219361879292051051L;
+	
+	public PublicKey pubKey;
+	public byte[] signature;
 	public Vector<String> ids;
-	public Header(){
-		ids = new Vector<String>();
-	}
-	public Header(Vector<String> newIds){
+
+	public Header(PublicKey pk, byte[] sig, Vector<String> newIds){
+		pubKey = pk;
+		signature = sig;
 		ids = newIds;
 	}
 	
